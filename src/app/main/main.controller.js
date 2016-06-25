7549e996b400ebfd4548f50be2bc771210680cfd
Kageetai <ma@kageetai.net>
2016-06-25 15:51:51 +0200

    mainController clean up

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,27 +1,10 @@
 export class MainController {
-  constructor ($timeout, SwapiApi, toastr) {
+  constructor (SwapiApi, toastr) {
     'ngInject';
 
-    this.people = [];
-    this.classAnimation = '';
     this.creationDate = 1466857363217;
     this.toastr = toastr;
 
-    this.activate($timeout, SwapiApi);
-  }
-
-  activate($timeout, SwapiApi) {
-    this.getPeople(SwapiApi);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
-
-  getPeople(SwapiApi) {
     this.people = SwapiApi.people.query();
-    //
-    // angular.forEach(this.people, (awesomeThing) => {
-    //   awesomeThing.rank = Math.random();
-    // });
   }
 }
